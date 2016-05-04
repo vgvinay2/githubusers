cd031e5f71b865240a1bf02c184b921e2e54dd7a
sevanti <sevanti.gouda@appbrowzer.com>
2016-05-04 16:45:02 +0530

    completed

--- app/routes/userlist.js
+++ app/routes/userlist.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	filter:null,
 	ajax: Ember.inject.service(),
 	model() {
-	return this.get('ajax').request('https://api.github.com/users');
+		return this.get('ajax').request('https://api.github.com/users');
 	},
+
 	actions: {
-		search(filter){
-			console.log( "i am here" + filter);
-			var filter = this.get('filter');
-			alert(filter);		
-		}
-	}
+	  search:function(stuff) {
+	  	var searchRegexp = new RegExp(stuff, "i");
+	  	var self = this;
+		  this.get('ajax').request('https://api.github.com/users').then(function (result) {
+		  	var searchResult = result.filter(function (user) {
+		  		return user.login.match(searchRegexp)
+		  	});
+		  	self.controller.set('model', searchResult);
+		  });
+	  }
+  }
 });
